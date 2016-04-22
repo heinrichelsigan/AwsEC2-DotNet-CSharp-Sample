8d623edc751e0ce9bdab6ab0368b99facb5313c0
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2016-04-22 18:49:50 +0200

    Update AssemblyInfo.cs

--- AwsEC2Sample1/Properties/AssemblyInfo.cs
+++ AwsEC2Sample1/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Amazon EC2 Sample")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Amazon EC2 Sample")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("AWS .NET SDK")]
 [assembly: AssemblyCompany("Amazon.com, Inc")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.*")]
+[assembly: AssemblyFileVersion("1.0.3.*")]
